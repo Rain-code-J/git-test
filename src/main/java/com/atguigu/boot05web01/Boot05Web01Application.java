1c0351b5749928715e00423ce00e46018d435ad6
Rain <junzhuo_dong@163.com>
2023-01-19 13:47:50 +0800

    gitee

--- src/main/java/com/atguigu/boot05web01/Boot05Web01Application.java
+++ src/main/java/com/atguigu/boot05web01/Boot05Web01Application.java
@@ -8,13 +8,6 @@ public class Boot05Web01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot05Web01Application.class, args);
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello master test");
-        System.out.println("hello hot-fix test");
-        System.out.println("hello push test");
-        System.out.println("hello pull test");
 
     }
 
